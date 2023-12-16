06752b3b9ae1498ba21f0ae4780ee23bf65ed4ee
Nguyen Viet Quang <quangviet932@gmail.com>
2023-12-16 14:00:40 +0700

    add monster hurt

--- Assets/Scripts/Characters/Monsters/Monster_Animator.cs
+++ Assets/Scripts/Characters/Monsters/Monster_Animator.cs
@@ -1,30 +1,18 @@
-using Sirenix.OdinInspector;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class Human_Animator : MonoBehaviour
+using Sirenix.OdinInspector;
+public class Monster_Animator : MonoBehaviour
 {
-    [SerializeField] private Animator animator;
+    [SerializeField] Animator animator;
     public enum AnimationType
     {
         Idle,
-        Walk,           
-        Slash,
         Hurt,
-    }
-
-    public void Slashing()
-    {
-        PlayAnimation(AnimationType.Slash);
     }
     public void Idle()
     {
         PlayAnimation(AnimationType.Idle);
-    }
-    public void Walking()
-    {
-        PlayAnimation(AnimationType.Walk);
     }
     public void Hurt()
     {
@@ -33,29 +21,23 @@ public class Human_Animator : MonoBehaviour
     [Button(ButtonSizes.Medium, ButtonStyle.FoldoutButton)]
     private void PlayAnimation(AnimationType type)
     {
-        if (animator == null)
+        var animationName = GetAnimationName(type);
+        if(animator == null)
         {
             animator = GetComponentInChildren<Animator>();
         }
-        var animationName = GetAnimationName(type);
         animator.Play(animationName);
-
     }
     string GetAnimationName(AnimationType type)
     {
         switch (type)
         {
             case AnimationType.Idle:
-                return "Idle_1";
-            case AnimationType.Walk:
-                return "walking_1";
-            case AnimationType.Slash:
-                return "slashing_1";
+                return "idle_1";
             case AnimationType.Hurt:
                 return "hurt_1";
             default:
-                break;
+                return "";
         }
-        return null;
     }
 }
