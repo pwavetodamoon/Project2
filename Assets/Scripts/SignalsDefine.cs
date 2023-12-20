45f0586268b0a5eebb88976888392c9c8716b461
Hồ Hữu Phước <hophuoc56@gmail.com>
2023-12-20 08:48:59 +0700

    code ui

---
+++ Assets/Scripts/SignalsDefine.cs
@@ -0,0 +1,10 @@
+using deVoid.Utils;
+public class OpenUIGamePlay : ASignal { }
+public class HideUIGamePlay : ASignal { }
+public class OpenUIMainMenu : ASignal { }
+public class HideUIMainMenu : ASignal { }
+
+public class ShowDPSMenu : ASignal { }
+public class HideDPSMenu : ASignal { }
+
+
---
+++ Assets/UIGamePlay.cs
@@ -0,0 +1,32 @@
+using deVoid.UIFramework;
+using deVoid.Utils;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIGamePlay : APanelController
+{
+    public Button ButtonShowDPS;
+    public Button ButtonHidesDPS;
+
+    protected override void AddListeners()
+    {
+        base.AddListeners();
+        ButtonShowDPS.onClick.AddListener(ShowDPS);
+    }
+
+    protected override void RemoveListeners()
+    {
+        base.RemoveListeners();
+        ButtonShowDPS.onClick.RemoveListener(ShowDPS);
+    }
+
+    private void ShowDPS()
+    {
+        Signals.Get<ShowDPSMenu>().Dispatch();
+    }
+
+    // private void HideDPS()
+    // {
+    //     Signals.Get<HideDPSMenu>().Dispatch();
+    // }
+}
