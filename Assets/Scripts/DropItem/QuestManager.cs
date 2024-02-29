b3f1a70316d20cb3f09f5872945e066f1c0bef58
Nguyễn Việt Quang <115866288+GhostZragon@users.noreply.github.com>
2024-02-29 17:45:31 +0700

    Complete New Quest logic

--- Assets/Scripts/DropItem/QuestManager.cs
+++ Assets/Scripts/DropItem/QuestManager.cs
@@ -1,20 +1,31 @@
 using System;
+using System.Collections.Generic;
 using Helper;
+using Sirenix.OdinInspector;
 using UnityEngine;
 
 namespace DropItem
 {
+    [Serializable]
+    public struct ItemsStruct
+    {
+        public int pointCollect;
+        public ItemsSO itemsSO;
+    }
+
     public class QuestManager : Singleton<QuestManager>
     {
-        [SerializeField] private ItemsSO questItem;
-        [SerializeField] private string idItemNeedCollect => questItem.Name;
+        public List<ItemsStruct> questItem;
+
+        //[SerializeField] private string idItemNeedCollect => questItem.Name;
         [Header("Quest Information")]
         [SerializeField] private int questItemInScene = 0;
+
         [SerializeField] private int itemNeedCollect = 5;
         [SerializeField] private int maxItemNeedCollect = 5;
-        public int QuestItemInScene { get { return questItemInScene; } set { questItemInScene = value; } }
-
-        public bool CanSpawnQuestItem => questItemInScene < maxItemNeedCollect;
+        [SerializeField] private int pointCollected = 0;
+        [SerializeField] private int pointNeedCollect = 100;
+        public bool CanSpawnQuestItem => true;
 
         public Action<ItemsSO> OnChangedQuestItem; // Can use for UI and logic game
 
@@ -23,28 +34,39 @@ namespace DropItem
             OnChangedQuestItem = null;
         }
 
-        public void ChangeQuestItem(ItemsSO newItemQuest)
+        public ItemsStruct GetRandomItemQuest()
+        {
+            var randomIndex = UnityEngine.Random.Range(0, questItem.Count);
+            return questItem[randomIndex];
+        }
+
+        public void ChangeQuestItem(List<ItemsStruct> newItemQuest)
         {
             if (newItemQuest == null) return;
             questItem = newItemQuest;
             itemNeedCollect = maxItemNeedCollect;
             questItemInScene = 0;
-            OnChangedQuestItem?.Invoke(questItem);
         }
 
-        public void OnCollectItem(string itemId)
+        public void OnCollectItem(string itemId, int newPoint)
         {
             if (questItem == null) return;
-            if (itemId != idItemNeedCollect) return;
+            //if (itemId != idItemNeedCollect) return;
 
             Debug.Log("We collect: " + itemId);
             Debug.Log("Item need collect is: " + (itemNeedCollect - 1));
             itemNeedCollect--;
-
+            pointCollected += newPoint;
             if (itemNeedCollect == 0)
             {
                 Debug.Log("We fine the quest");
             }
+        }
+
+        [Button]
+        public float GetCompletePercent()
+        {
+            return (float) pointCollected / pointNeedCollect;
         }
     }
 }
