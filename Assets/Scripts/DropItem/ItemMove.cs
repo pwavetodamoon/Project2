603cbb829f1b646f10a02535970c59f58b27ca25
Nguyễn Việt Quang <115866288+GhostZragon@users.noreply.github.com>
2024-03-01 19:07:20 +0700

    WIP

--- Assets/Scripts/DropItem/ItemMove.cs
+++ Assets/Scripts/DropItem/ItemMove.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class ItemMove : MonoBehaviour
 {
-    public float speed = 5f;
+    public float speed = 3f;
     public Vector2 moveVector = Vector2.left;
     public void Update()
     {
