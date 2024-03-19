1708358713aa0f8072bf7ed28e22514c5ea1eafe
Nguyễn Việt Quang <115866288+GhostZragon@users.noreply.github.com>
2024-03-19 14:11:37 +0700

    WIP

--- Assets/Scripts/UIScript/UIGamePlayController.cs
+++ Assets/Scripts/UIScript/UIGamePlayController.cs
@@ -40,12 +40,12 @@ public class UIGamePlayController : MonoBehaviour
         //------
         Signals.Get<OpenUIMainMenu>().RemoveListener(ShowUIMainMenu);
         Signals.Get<HideUIMainMenu>().RemoveListener(HideUIMainMenu);
-        
+
         //
         Signals.Get<OpenUISoundSetting>().AddListener(ShowUISoundSetting);
         Signals.Get<CloseUISoundSetting>().AddListener(HideUISoundSetting);
-        
-        
+
+
         //
         Signals.Get<OpenUISoundSetting>().RemoveListener(ShowUISoundSetting);
         Signals.Get<CloseUISoundSetting>().RemoveListener(HideUISoundSetting);
@@ -68,11 +68,12 @@ public class UIGamePlayController : MonoBehaviour
     {
         _uIFrameGamePlay.HidePanel(ScreenIds.UIMainMenu);
     }
-    
+
     private void ShowUISoundSetting()
     {
         _uIFrameGamePlay.ShowPanel(ScreenIds.UISoundSetting);
-    }private void HideUISoundSetting()
+    }
+    private void HideUISoundSetting()
     {
         _uIFrameGamePlay.HidePanel(ScreenIds.UISoundSetting);
     }
