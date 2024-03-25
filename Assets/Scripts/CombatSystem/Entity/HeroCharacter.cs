f7d369c3f1e2a60f8a7c54fb3383cfb2d10461b2
Nguyễn Việt Quang <115866288+GhostZragon@users.noreply.github.com>
2024-03-25 13:36:22 +0700

    WIP

--- Assets/Scripts/CombatSystem/Entity/HeroCharacter.cs
+++ Assets/Scripts/CombatSystem/Entity/HeroCharacter.cs
@@ -6,6 +6,7 @@ using LevelAndStats;
 using Model.Hero;
 using Model.Monsters;
 using SlotHero;
+using SortingLayers;
 using UnityEngine;
 
 namespace CombatSystem.Entity
@@ -20,6 +21,7 @@ namespace CombatSystem.Entity
         private Character_Body_Sprites sprites;
         private Animator_Base animatorBase;
         public bool IsDead;
+        public SortingLayerByYAxis sortingLayerByYAxis;
 
         public int InGameSlotIndex { get; private set; }
 
@@ -31,7 +33,7 @@ namespace CombatSystem.Entity
             gameObject.layer = LayerMask.NameToLayer(GameLayerMask.Hero);
             sprites = GetComponentInChildren<Character_Body_Sprites>();
             animatorBase = GetComponentInChildren<Animator_Base>();
-
+            sortingLayerByYAxis = GetComponent<SortingLayerByYAxis>();
             entityStateManager.OnDie += OnDead;
             entityStateManager.OnRebirth += OnRebirth;
         }
